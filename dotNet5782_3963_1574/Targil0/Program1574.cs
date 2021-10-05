37234fa8d59fa9d31d8b69c505fccae500640260
rinatwalles <rinatwalles@gmail.com>
2021-10-05 16:56:51 +0300

    initial code+welcome function

---
+++ dotNet5782_3963_1574/Targil0/Program1574.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome1574();
+            Welcome3963();
+            Console.ReadKey();
+        }
+
+        static partial void Welcome3963();
+        private static void Welcome1574()
+        {
+            Console.Write("Enter your name: ");
+            string name = System.Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", name);
+        }
+    }
+}
