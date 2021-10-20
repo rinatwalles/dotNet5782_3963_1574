ed13a5c71e96ad8d09a630be4493c2e8876a50f2
rinatwalles <rinatwalles@gmail.com>
2021-10-20 13:33:49 +0300

    orgenizing the structs

---
+++ DAL/Drone.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace IDAL
+{
+    namespace DO
+    {
+        struct Drone
+        {
+            public int Id { get; set; }
+            public string Model { get; set; }
+            public WeightCategories MaxWeight { get; set; }
+            public DroneStatuses Status { get; set; }
+            public double Battery { get; set; }
+        }
+
+
+    }
+
+}
+
+
+
+
