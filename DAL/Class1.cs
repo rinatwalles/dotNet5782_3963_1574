dec6f891558475d38d9b64425ea770b95f3e635f
YaelMetz1 <yaelmetz21@gmail.com>
2021-10-20 12:54:19 +0300

    adding 4/5 structs to DAL

--- DAL/Class1.cs
+++ DAL/Class1.cs
@@ -4,10 +4,38 @@ namespace IDAL
 {
     namespace DO
     {
-        public struct
+        struct Station
         {
-
+            public int Id { get; set;}
+            public int Name { get; set; }
+            public double Longitude { get; set; }
+            public double Latitude { get; set; }
+            public int ChargeSlots { get; set; }
         }
+
+        struct Drone
+        {
+            public int Id { get; set; }
+            public string Model { get; set; }
+            public WeightCategories MaxWeight { get; set; }
+            public DroneStatuses Status { get; set; }
+            public double Battery { get; set; }
+        }
+
+        struct DroneCharge
+        {
+            public int DroneId { get; set; }
+            public int StationId { get; set; }
+        }
+        struct Customer
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Phone { get; set; }
+            public double Longitude { get; set; }
+            public double Latitude { get; set; }
+        }
+
     }
 
 }
