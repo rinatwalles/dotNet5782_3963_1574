1d1ba4389e30ce0997b2b1ca544b9acb0bd598d3
YaelMetz1 <yaelmetz21@gmail.com>
2021-11-14 14:56:52 +0200

    targil2-5

---
+++ BL/CustomerOfParcel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    namespace IBL.BO
+    {
+        public class CustomerOfParcel
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+        }
+    }
+}
