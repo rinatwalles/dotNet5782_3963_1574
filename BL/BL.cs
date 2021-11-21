bc20d6e0ce02186839e19e236465274ebb7e35cb
rinatwalles <rinatwalles@gmail.com>
2021-11-21 11:25:55 +0200

    mimshak

---
+++ BL/BL.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DalObject;
+using IDAL.DO;
+
+namespace BL
+{
+    public class BL
+    {
+        //IDAL.DalObject idal = new IDAL.DalObject();
+        DalObject.DalObject dall = new DalObject.DalObject();
+
+        
+    }
+}
