8474ca3f94520947b8087617be9c368a89b4fd4f
rinatwalles <rinatwalles@gmail.com>
2021-12-13 16:59:11 +0200

    boyo

--- PL/DroneList.xaml.cs
+++ PL/DroneList.xaml.cs
@@ -10,16 +10,17 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace PL
 {
     /// <summary>
-    /// Interaction logic for DroneList.xaml
+    /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class DroneList : Window
+    public partial class MainWindow : Window
     {
-        public DroneList()
+        public MainWindow()
         {
             InitializeComponent();
         }
