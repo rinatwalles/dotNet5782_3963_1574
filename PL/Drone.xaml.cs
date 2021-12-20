8625324c2303baedf4e1ad20b92052eb9c59523d
YaelMetz1 <yaelmetz21@gmail.com>
2021-12-20 22:04:13 +0200

    1234556

--- PL/Drone.xaml.cs
+++ PL/Drone.xaml.cs
@@ -20,23 +20,30 @@ namespace PL
     public partial class Drone : Window
     {
         private IBL.IBL ibl;
+        IBL.BO.DroneToList PLDrone;
         public Drone(IBL.IBL newIbl)    //add constructor
         {
             InitializeComponent();
             ibl = newIbl;
+
+            PLDrone = new IBL.BO.DroneToList();
 
             StatusComboBox.ItemsSource = Enum.GetValues(typeof(IBL.BO.DroneStatuses));
             WeightComboBox.ItemsSource = Enum.GetValues(typeof(IBL.BO.WeightCategories));
             this.Title = "Drone Addition";
             OptionButtun.Content = "ADD The Drone";
 
-
+            ParcelIDLabel.Visibility = txtParcelID.Visibility = Visibility.Collapsed;
+            BatteryLabel.Visibility = txtBattery.Visibility = Visibility.Collapsed;
+            StatusLabel.Visibility = StatusComboBox.Visibility = Visibility.Collapsed;
         }
 
-        public Drone(IBL.IBL newIbl, IBL.BO.Drone d) //update constructor
+        public Drone(IBL.IBL newIbl, IBL.BO.DroneToList d) //update constructor
         {
             InitializeComponent();
             ibl = newIbl;
+
+            PLDrone = d;
 
             StatusComboBox.ItemsSource = Enum.GetValues(typeof(IBL.BO.DroneStatuses));
             WeightComboBox.ItemsSource = Enum.GetValues(typeof(IBL.BO.WeightCategories));
@@ -51,6 +58,36 @@ namespace PL
         }
 
         private void txtParcelID_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void txtID_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void WeightComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            for (int i = 0; i < 3; ++i)
+            {
+                ListBoxItem newItem = new ListBoxItem();
+                newItem.Content = (IBL.BO.WeightCategories)i;
+                WeightComboBox.Items.Add(newItem);
+            }
+        }
+
+        private void StatusComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            for (int i = 0; i < 3; ++i)
+            {
+                ListBoxItem newItem = new ListBoxItem();
+                newItem.Content = (IBL.BO.DroneStatuses)i;
+                StatusComboBox.Items.Add(newItem);
+            }
+        }
+
+        private void txtModel_TextChanged(object sender, TextChangedEventArgs e)
         {
 
         }
