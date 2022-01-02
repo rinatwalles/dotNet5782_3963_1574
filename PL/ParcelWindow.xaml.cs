969cef217fd6bdc69c1b3c8c80da10bc90535cff
rinatwalles <rinatwalles@gmail.com>
2022-01-02 18:27:20 +0200

    parcel

--- PL/ParcelWindow.xaml.cs
+++ PL/ParcelWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,9 +20,38 @@ namespace PL
     /// </summary>
     public partial class ParcelWindow : Window
     {
-        public ParcelWindow()
+        private BLApi.IBL ibl;
+        enum option { Add, Update };
+        option op;
+        public ParcelWindow(BLApi.IBL newIbl)
         {
             InitializeComponent();
+            ibl = newIbl;
+            Parcel parcel = new Parcel();
+            op = option.Add;
+
+            priorityComboBox.ItemsSource = Enum.GetValues(typeof(Priorities));
+            weightComboBox.ItemsSource = Enum.GetValues(typeof(WeightCategories));
+
+            IEnumerable<StationToList> lst = ibl.GetAllStationsWithAvailableSlots();
+            var txt = from StationToList item in lst
+                      select item.Id;
+            //stationComboBox.ItemsSource = txt;
+
+
+            //this.Title = "Drone Addition";
+            //OptionButtun.Content = "ADD The Drone";
+
+            //ParcelIDLabel.Visibility = txtParcelID.Visibility = Visibility.Collapsed;
+            //BatteryLabel.Visibility = txtBattery.Visibility = Visibility.Collapsed;
+            //StatusLabel.Visibility = StatusComboBox.Visibility = Visibility.Collapsed;
+            //latitudeTextBox.Visibility = latitudeLabel.Visibility = Visibility.Collapsed;
+            //longitudeTextBox.Visibility = longitudeLabel.Visibility = Visibility.Collapsed;
+
+            //UpdateButton.Visibility = Visibility.Collapsed;
+            //DeliveryButton.Visibility = Visibility.Collapsed;
+
+            //OptionButtun.IsEnabled = false;
         }
     }
 }
