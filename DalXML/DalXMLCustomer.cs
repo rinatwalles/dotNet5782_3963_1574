c746604563675f66cc46ce586334aa6fa432112c
rinatwalles <rinatwalles@gmail.com>
2022-01-09 12:41:50 +0200

    partial

---
+++ DalXML/DalXMLCustomer.cs
@@ -0,0 +1,59 @@
+﻿using DaLApi;
+using DO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dal
+{
+    partial class DalXML:IDAL
+    {
+
+        public bool CheckCustomer(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CustomerAddition(Customer c)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Customer GetCustomer(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CustomerCollecting(int parcelId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Customer> AllCustomer()
+        {
+            throw new NotImplementedException();
+        }
+
+        public double DistanceFromCustomer(int id, double x1, double y1)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CustomerDelete(Customer c)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CustomerUpdate(Customer c)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Customer> GetCustomerInParcelByPredicate(Predicate<Customer> predicate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DalXML/DalXMLParcel.cs
@@ -0,0 +1,61 @@
+﻿using DaLApi;
+using DO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dal
+{
+    partial class DalXML:IDAL
+    {
+        public bool CheckParcel(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ParcelAddition(Parcel p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Parcel GetParcel(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Parcel> AllParcel()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ParcelDelete(Parcel p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Parcel getParcelByDroneId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ParcelUpdate(Parcel p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Parcel> GetParcelByPredicate(Predicate<Parcel> predicate)
+        {
+            throw new NotImplementedException();
+        }
+        public Parcel GetOneParcelByPredicate(Predicate<Parcel> predicate)
+        {
+            throw new NotImplementedException();
+        }
+        public IEnumerable<Drone> GetDroneInParcelByPredicate(Predicate<Drone> predicate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
