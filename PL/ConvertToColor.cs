5ad1da5990144446dec34d081168c5f93bd09d41
YaelMetz1 <yaelmetz21@gmail.com>
2022-02-19 18:59:53 +0200

    saving

---
+++ PL/ConvertToColor.cs
@@ -0,0 +1,33 @@
+﻿using DocumentFormat.OpenXml.Drawing.Diagrams;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace PL
+{
+   public class ConvertToColor : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+
+            double battery = (double)value;
+            if (battery < 20)
+                return Backround.Red;
+            else if (battery < 40)
+                return Background.Red;
+            else if (battery < 60)
+                return Visibility.Visible;
+            else
+                return Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
